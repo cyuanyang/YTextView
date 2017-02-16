4a0dec1dac7aa503f7b826c725079e4a3d95fd1e
yuanyang <yuanyang.chen@xstudy.com.cn>
2017-02-16 15:07:09 +0800

    截图

--- app/src/main/java/com/cyy/ytextviewsimple/MainActivity.java
+++ app/src/main/java/com/cyy/ytextviewsimple/MainActivity.java
@@ -1,29 +1,14 @@
 package com.cyy.ytextviewsimple;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.cyy.ytextview.Item;
-import com.cyy.ytextview.Listener;
-import com.cyy.ytextview.Text;
-import com.cyy.ytextview.YTextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,12 +16,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener , MyAdapter.CommentCallback , CommentHelper.Callback{
 
     protected ListView listView;
-//    protected EditText editText;
-//    protected Button completeBtn;
     protected TextView textView;
     private MyAdapter adapter;
 
-    private List<List<Item>> items = new ArrayList<>();
     private List<Model> datas = new ArrayList<>();
 
     private CommentHelper commentHelper;
@@ -67,9 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         listView = (ListView) findViewById(R.id.listView);
-//        editText = (EditText) findViewById(R.id.editText);
-//        completeBtn = (Button) findViewById(R.id.completeBtn);
-//        completeBtn.setOnClickListener(MainActivity.this);
         textView = (TextView) findViewById(R.id.textView);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
@@ -80,50 +59,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         commentHelper.comment(position);
     }
 
-//    class Clickable extends ClickableSpan implements View.OnClickListener {
-//        private final View.OnClickListener mListener;
-//
-//        public Clickable(View.OnClickListener l) {
-//            mListener = l;
-//        }
-//
-//        @Override
-//        public void onClick(View v) {
-//            mListener.onClick(v);
-//        }
-//    }
 
     @Override
     public void onClick(View view) {
-//        if (view.getId() == R.id.completeBtn) {
-//            Text text = new Text();
-//            SpannableString spannableString = new SpannableString(editText.getText().toString());
-//            spannableString.setSpan(new Clickable(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    //点击代码
-//                    Toast.makeText(MainActivity.this, "onClick", Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//            spannableString.setSpan(new ForegroundColorSpan(Color.RED), 0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//            text.text = spannableString;
-//            text.setOnItemTapListener(new Listener.OnItemTapListener() {
-//                @Override
-//                public void onItemTapListener(View view, int position) {
-//                    Toast.makeText(MainActivity.this, MainActivity.this.items.get(0).get(position).getPosition() + "", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//            if (items.size() == 0) {
-//                List<Item> item = new ArrayList<>();
-//                item.add(text);
-//                items.add(item);
-//            } else {
-//                items.get(0).add(text);
-//            }
-//            adapter.notifyDataSetChanged();
-//        }
     }
 
 
